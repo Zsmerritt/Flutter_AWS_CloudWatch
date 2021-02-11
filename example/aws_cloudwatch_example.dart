26cb8f1ec19312cf84383bb4ad0ffc5c28bf6f61
=Zachary Merritt <=zsmerritt@gmail.com>
2021-02-10 16:37:15 -0800

    added example

---
+++ example/aws_cloudwatch_example.dart
@@ -0,0 +1,17 @@
+import 'package:aws_cloudwatch/aws_cloudwatch.dart';
+
+// AWS Variables
+const String AWS_ACCESS_KEY_ID = 'ExampleKey';
+const String AWS_SECRET_ACCESS_KEY = 'ExampleSecret';
+const String Region = 'us-west-2';
+const String ServiceInstance = 'Logs_XXXXXXXX';
+
+// Logging Variables
+const String LogGroup = 'LogGroupExample';
+String logStreamName = 'LogStreamExample';
+CloudWatch cloudWatch = new CloudWatch(
+    AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, Region, ServiceInstance);
+
+void log(String logString) {
+  cloudWatch.log(logString);
+}
